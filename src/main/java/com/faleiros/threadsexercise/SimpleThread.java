c4e5623d6daa4a25508a582c1d362775ebc753a7
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-08-30 22:38:14 -0300

    Creating simple single thread

--- src/main/java/com/faleiros/threadsexercise/SimpleThread.java
+++ src/main/java/com/faleiros/threadsexercise/SimpleThread.java
@@ -9,9 +9,11 @@ package com.faleiros.threadsexercise;
  *
  * @author josiel
  */
-public class Main {
-    public static void main(String[] args) {
-        Thread thread = new Thread(new SimpleThread());
-        thread.start();
+public class SimpleThread implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("My Simple Thrad is running");
     }
+    
 }
